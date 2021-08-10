424ccb8335c0233b4adf6126fb33dd3b2fe01521
Ilyasse <elias.faraone@outlook.com>
2021-08-10 13:03:10 +0100

    Implemented neuro-decoder in PyTorch

--- neuro-stabiliser/neuro-stabilizer.py
+++ neuro-stabiliser/neuro-stabilizer.py
@@ -1,105 +1,282 @@
-from netpyne import specs
-
-###############################################################################
-# NETWORK PARAMETERS
-###############################################################################
-
-netParams = specs.NetParams()  # object of class NetParams to store the network parameters
-
-
-# Cell parameters
-## PYR cell properties
-secs = {}
-secs['soma'] = {'geom': {}, 'topol': {}, 'mechs': {}}  # soma properties
-secs['soma']['geom'] = {'diam': 18.8, 'L': 18.8}
-secs['soma']['mechs']['hh'] = {'gnabar': 0.12, 'gkbar': 0.036, 'gl': 0.003, 'el': -70}
-netParams.cellParams['PYR'] = {'secs': secs}  # add dict to list of cell properties
-
-# Population parameters
-netParams.popParams['hop'] = {'cellType': 'PYR', 'numCells': 50}     # add dict with params for this pop
-#netParams.popParams['background'] = {'cellModel': 'NetStim', 'rate': 50, 'noise': 0.5}  # background inputs
-
-# Synaptic mechanism parameters
-netParams.synMechParams['exc'] = {'mod': 'Exp2Syn', 'tau1': 0.1, 'tau2': 1.0, 'e': 0}
-netParams.synMechParams['inh'] = {'mod': 'Exp2Syn', 'tau1': 0.1, 'tau2': 1.0, 'e': -80}
+# import os
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.gridspec import GridSpec
+# import seaborn as sns
+import torch
+import torch.nn as nn
 
 
-# Stimulation parameters
-netParams.stimSourceParams['bkg'] = {'type': 'NetStim', 'rate': 50, 'noise': 0.5}
-netParams.stimTargetParams['bkg->all'] = {'source': 'bkg', 'conds': {'pop': 'hop'}, 'weight': 0.1, 'delay': 1, 'synMech': 'exc'}
+
+nb_inputs  = 100
+nb_hidden  = 4
+nb_outputs = 2
+time_step = 1e-3
+nb_steps  = 200
+batch_size = 256
+dtype = torch.float
+device = torch.device("cpu")
+
+# Uncomment the line below to run on GPU
+# device = torch.device("cuda:0")
+
+## DATASET
+
+freq = 5 # Hz
+prob = freq*time_step
+mask = torch.rand((batch_size,nb_steps,nb_inputs), device=device, dtype=dtype)
+x_data = torch.zeros((batch_size,nb_steps,nb_inputs), device=device, dtype=dtype, requires_grad=False)
+x_data[mask<prob] = 1.0
+## RANDOM LABEL
+y_data = torch.tensor(1*(np.random.rand(batch_size)<0.5), device=device)
 
 
-# Connectivity parameters
-netParams.connParams['hop->hop'] = {
-    'preConds': {'pop': 'hop'},
-    'postConds': {'pop': 'hop'},
-    'weight': 0.0,                      # weight of each connection
-    'synMech': 'inh',                   # target inh synapse
-    'delay': 5}                         # delay
+## SPIKING MODEL SEETUP
+
+tau_mem = 10e-3
+tau_syn = 5e-3
+alpha   = float(np.exp(-time_step/tau_syn))
+beta    = float(np.exp(-time_step/tau_mem))
+weight_scale = 7*(1.0-beta) # this should give us some spikes to begin with
+
+w1 = torch.empty((nb_inputs, nb_hidden),  device=device, dtype=dtype, requires_grad=True)
+torch.nn.init.normal_(w1, mean=0.0, std=weight_scale/np.sqrt(nb_inputs))
+
+w2 = torch.empty((nb_hidden, nb_outputs), device=device, dtype=dtype, requires_grad=True)
+torch.nn.init.normal_(w2, mean=0.0, std=weight_scale/np.sqrt(nb_hidden))
+
+print("init done")
+
+# multiply all input spikes with the weight matrix. 
+# We have to do this for each time step in each input example in the batch. 
+# Because we have stored our input spikes in a rank three tensor we can 
+# express this operation in a single line:
+
+h1 = torch.einsum("abc,cd->abd", (x_data, w1))
+
+# The spiking non-linearity - NAIVE
+
+def spike_fn(x):
+    out = torch.zeros_like(x)
+    out[x > 0] = 1.0
+    return out
 
 
-###############################################################################
-# SIMULATION PARAMETERS
-###############################################################################
-simConfig = specs.SimConfig()  # object of class SimConfig to store simulation configuration
+def plot_voltage_traces(mem, spk=None, dim=(3,5), spike_height=5):
+    gs=GridSpec(*dim)
+    if spk is not None:
+        dat = 1.0*mem
+        dat[spk>0.0] = spike_height
+        dat = dat.detach().cpu().numpy()
+    else:
+        dat = mem.detach().cpu().numpy()
+    for i in range(np.prod(dim)):
+        if i==0: a0=ax=plt.subplot(gs[i])
+        else: ax=plt.subplot(gs[i],sharey=a0)
+        ax.plot(dat[i])
+        ax.axis("off")
 
-# Simulation options
-simConfig.duration = 0.5*1e3        # Duration of the simulation, in ms
-simConfig.dt = 0.025                # Internal integration timestep to use
-simConfig.verbose = False           # Show detailed messages
-simConfig.recordTraces = {'V_soma':{'sec':'soma','loc':0.5,'var':'v'}}  # Dict with traces to record
-simConfig.recordStep = 1            # Step size in ms to save data (eg. V traces, LFP, etc)
-simConfig.filename = 'model_output' # Set file output name
-simConfig.savePickle = False        # Save params, network and sim output to pickle file
+syn = torch.zeros((batch_size,nb_hidden), device=device, dtype=dtype)
+mem = torch.zeros((batch_size,nb_hidden), device=device, dtype=dtype)
 
-simConfig.analysis['plotRaster'] = {'syncLines': True, 'saveFig': True}      # Plot a raster
-simConfig.analysis['plotTraces'] = {'include': [1], 'saveFig': True}         # Plot recorded traces for this list of cells
-simConfig.analysis['plot2Dnet'] = {'saveFig': True}                          # plot 2D cell positions and connections
+# Here we define two lists which we use to record the membrane potentials and output spikes
+mem_rec = []
+spk_rec = []
+
+# Here we loop over time
+for t in range(nb_steps):
+    mthr = mem-1.0
+    out = spike_fn(mthr)
+    rst = out.detach() # We do not want to backprop through the reset
+
+    new_syn = alpha*syn +h1[:,t]
+    new_mem = (beta*mem +syn)*(1.0-rst)
+    
+    mem_rec.append(mem)
+    spk_rec.append(out)
+    
+    mem = new_mem
+    syn = new_syn
+
+# Now we merge the recorded membrane potentials into a single tensor
+mem_rec = torch.stack(mem_rec,dim=1)
+spk_rec = torch.stack(spk_rec,dim=1)
+
+def run_snn(inputs):
+    h1 = torch.einsum("abc,cd->abd", (inputs, w1))
+    syn = torch.zeros((batch_size,nb_hidden), device=device, dtype=dtype)
+    mem = torch.zeros((batch_size,nb_hidden), device=device, dtype=dtype)
+
+    mem_rec = []
+    spk_rec = []
+
+    # Compute hidden layer activity
+    for t in range(nb_steps):
+        mthr = mem-1.0
+        out = spike_fn(mthr)
+        rst = out.detach() # We do not want to backprop through the reset
+
+        new_syn = alpha*syn +h1[:,t]
+        new_mem = (beta*mem +syn)*(1.0-rst)
+
+        mem_rec.append(mem)
+        spk_rec.append(out)
+        
+        mem = new_mem
+        syn = new_syn
+
+    mem_rec = torch.stack(mem_rec,dim=1)
+    spk_rec = torch.stack(spk_rec,dim=1)
+
+    # Readout layer
+    h2= torch.einsum("abc,cd->abd", (spk_rec, w2))
+    flt = torch.zeros((batch_size,nb_outputs), device=device, dtype=dtype)
+    out = torch.zeros((batch_size,nb_outputs), device=device, dtype=dtype)
+    out_rec = [out]
+    for t in range(nb_steps):
+        new_flt = alpha*flt +h2[:,t]
+        new_out = beta*out +flt
+
+        flt = new_flt
+        out = new_out
+
+        out_rec.append(out)
+
+    out_rec = torch.stack(out_rec,dim=1)
+    other_recs = [mem_rec, spk_rec]
+    return out_rec, other_recs
 
 
-###############################################################################
-# EXECUTION CODE (via netpyne)
-###############################################################################
-from netpyne import sim
-
-# Create network and run simulation
-sim.initialize(                     # create network object and set cfg and net params
-    simConfig = simConfig,          # pass simulation config and network params as arguments
-    netParams = netParams)
-sim.net.createPops()                # instantiate network populations
-sim.net.createCells()               # instantiate network cells based on defined populations
-sim.net.connectCells()              # create connections between cells based on params
-sim.net.addStims()                  # add stimulation
-sim.setupRecording()                # setup variables to record for each cell (spikes, V traces, etc)
-sim.runSim()                        # run parallel Neuron simulation
-sim.gatherData()                    # gather spiking data and cell info from each node
-sim.saveData()                      # save params, cell info and sim output to file (pickle,mat,txt,etc)
-sim.analysis.plotData()             # plot spike raster
+def print_classification_accuracy():
+    """ Dirty little helper function to compute classification accuracy. """
+    output,_ = run_snn(x_data)
+    m,_= torch.max(output,1) # max over time
+    _,am=torch.max(m,1) # argmax over output units
+    acc = np.mean((y_data==am).detach().cpu().numpy()) # compare to labels
+    print("Accuracy %.3f"%acc)
+    
+print_classification_accuracy()
 
 
-###############################################################################
-# INTERACTING WITH INSTANTIATED NETWORK
-###############################################################################
+## TRAINING - TRUE GRADIENT
 
-# modify conn weights
-sim.net.modifyConns({'conds': {'label': 'hop->hop'}, 'weight': 0.5})
+# params = [w1,w2] # The paramters we want to optimize
+# optimizer = torch.optim.Adam(params, lr=2e-3, betas=(0.9,0.999)) # The optimizer we are going to use
 
-sim.runSim()                          # run parallel Neuron simulation
-sim.gatherData()                      # gather spiking data and cell info from each node
-sim.saveData()                        # save params, cell info and sim output to file (pickle,mat,txt,etc)
-sim.analysis.plotData()               # plot spike raster
+# log_softmax_fn = nn.LogSoftmax(dim=1) # The log softmax function across output units
+# loss_fn = nn.NLLLoss() # The negative log likelihood loss function
 
-# modify cells geometry
-sim.net.modifyCells({'conds': {'pop': 'hop'},
-                    'secs': {'soma': {'geom': {'L': 160}}}})
+# # The optimization loop
+# loss_hist = []
+# for e in range(1000):
+#     # run the network and get output
+#     output,_ = run_snn(x_data) 
+#     # compute the loss
+#     m,_=torch.max(output,1)
+#     log_p_y = log_softmax_fn(m) 
+#     loss_val = loss_fn(log_p_y, y_data)
 
-sim.simulate()
+#     # update the weights
+#     optimizer.zero_grad()
+#     loss_val.backward()
+#     optimizer.step()
+    
+#     # store loss value
+#     loss_hist.append(loss_val.item())
+    
+# loss_hist_true_grad = loss_hist # store for later use
 
-from netpyne import __gui__
-if __gui__:
-    sim.analysis.plotRaster(syncLines=True)
-    sim.analysis.plotTraces(include = [1])
 
-# check model output
-sim.checkOutput('tut7')
+# plt.plot(loss_hist)
+# plt.xlabel("Epoch")
+# plt.ylabel("Loss")
+# sns.despine()
+# print_classification_accuracy()
 
+
+class SurrGradSpike(torch.autograd.Function):
+    """
+    Here we implement our spiking nonlinearity which also implements 
+    the surrogate gradient. By subclassing torch.autograd.Function, 
+    we will be able to use all of PyTorch's autograd functionality.
+    Here we use the normalized negative part of a fast sigmoid 
+    as this was done in Zenke & Ganguli (2018).
+    """
+    
+    scale = 100.0 # controls steepness of surrogate gradient
+
+    @staticmethod
+    def forward(ctx, input):
+        """
+        In the forward pass we compute a step function of the input Tensor
+        and return it. ctx is a context object that we use to stash information which 
+        we need to later backpropagate our error signals. To achieve this we use the 
+        ctx.save_for_backward method.
+        """
+        ctx.save_for_backward(input)
+        out = torch.zeros_like(input)
+        out[input > 0] = 1.0
+        return out
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        """
+        In the backward pass we receive a Tensor we need to compute the 
+        surrogate gradient of the loss with respect to the input. 
+        Here we use the normalized negative part of a fast sigmoid 
+        as this was done in Zenke & Ganguli (2018).
+        """
+        input, = ctx.saved_tensors
+        grad_input = grad_output.clone()
+        grad = grad_input/(SurrGradSpike.scale*torch.abs(input)+1.0)**2
+        return grad
+    
+# here we overwrite our naive spike function by the "SurrGradSpike" 
+# nonlinearity which implements a surrogate gradient
+spike_fn  = SurrGradSpike.apply
+
+# The following lines will reinitialize the weights
+torch.nn.init.normal_(w1, mean=0.0, std=weight_scale/np.sqrt(nb_inputs))
+torch.nn.init.normal_(w2, mean=0.0, std=weight_scale/np.sqrt(nb_hidden))
+print("init done")
+
+params = [w1,w2]
+optimizer = torch.optim.Adam(params, lr=2e-3, betas=(0.9,0.999))
+
+log_softmax_fn = nn.LogSoftmax(dim=1)
+loss_fn = nn.NLLLoss()
+
+loss_hist = []
+for e in range(1000):
+    output,_ = run_snn(x_data)
+    m,_=torch.max(output,1)
+    log_p_y = log_softmax_fn(m)
+    loss_val = loss_fn(log_p_y, y_data)
+
+    optimizer.zero_grad()
+    loss_val.backward()
+    optimizer.step()
+    loss_hist.append(loss_val.item())
+
+# plt.figure(figsize=(3.3,2),dpi=150)
+# plt.plot(loss_hist, label="Surrogate gradient")
+# plt.xlabel("Epoch")
+# plt.ylabel("Loss")
+# plt.legend()
+# sns.despine()
+
+
+output,_ = run_snn(x_data)
+m,_=torch.max(output,1)
+
+# Compute training accuracy
+_,am=torch.max(m,1)
+acc = np.mean((y_data==am).detach().cpu().numpy())
+print("Accuracy %f"%acc)
+
+
+
+# out_rec,other_recs = run_snn(x_data)
+
+# fig=plt.figure(dpi=100)
+# plot_voltage_traces(out_rec)
