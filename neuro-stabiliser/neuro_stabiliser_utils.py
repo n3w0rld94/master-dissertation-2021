6bf849a3e02f614d692d24c5df4adc1d119c69e3
Ilyasse <elias.faraone@outlook.com>
2021-08-15 22:03:50 +0100

    managed to get the controller to work

--- neuro-stabiliser/neuro_stabiliser_utils.py
+++ neuro-stabiliser/neuro_stabiliser_utils.py
@@ -3,41 +3,34 @@ from matplotlib import pyplot as plt
 import numpy as np
 import torch
 
-def plot_voltage_traces(mem, spk=None, dim=(3,5), spike_height=5):
-    gs=GridSpec(*dim)
+
+def plot_voltage_traces(mem, spk=None, dim=(3, 5), spike_height=5):
+    gs = GridSpec(*dim)
     if spk is not None:
         dat = 1.0*mem
-        dat[spk>0.0] = spike_height
+        dat[spk > 0.0] = spike_height
         dat = dat.detach().cpu().numpy()
     else:
         dat = mem.detach().cpu().numpy()
     for i in range(np.prod(dim)):
-        if i==0: a0=ax=plt.subplot(gs[i])
-        else: ax=plt.subplot(gs[i],sharey=a0)
+        if i == 0:
+            a0 = ax = plt.subplot(gs[i])
+        else:
+            ax = plt.subplot(gs[i], sharey=a0)
         ax.plot(dat[i])
         ax.axis("off")
 
-def pre_process_data(device, orig_x_data, time_series_size, number_of_batches):
-    line_1 = np.split(orig_x_data[0], number_of_batches)
-    line_2 = np.split(orig_x_data[1], number_of_batches)
-    line_3 = np.split(orig_x_data[2], number_of_batches)
 
-    x_data = torch.tensor(np.concatenate([line_1, line_2, line_3]).tolist(), device = device)
+def pre_process_data(device, orig_x_data):
+    dtype = torch.float
+    device = torch.device("cpu")
 
-    # Generate labels
-    b = []
-    indexes = []
-    for i in range(number_of_batches):
-        b.append(0)
-        indexes.append(i * 0.8 * time_series_size)
+    x_data = orig_x_data.reshape(15, 10, 10).tolist()
 
-    for i in range(number_of_batches):
-        b.append(1)
-        indexes.append(i * 0.8 * time_series_size)
+    # # Generate labels
+    labels = np.concatenate([[0] * 5, [1] * 5, [2] * 5])
 
-    for i in range(number_of_batches):
-        b.append(2)
-        indexes.append(i * 0.8 * time_series_size)
+    x_data = torch.tensor(x_data, device=device, dtype=dtype, requires_grad=False)
+    y_data = torch.tensor(labels, device=device, dtype=torch.long)
+    return x_data, y_data
 
-    y_data = torch.tensor(b, device = device)
-    return x_data,y_data
